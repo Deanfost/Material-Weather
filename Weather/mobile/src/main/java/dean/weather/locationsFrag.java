7a4ca8ce8faedda46100e634692a2ada04cfeb04
Dean Foster <Deanfoster11@gmail.com>
2016-09-26 20:44:43 -0400

    Begining work on locations fragment

--- Weather/mobile/src/main/java/dean/weather/locationsFrag.java
+++ Weather/mobile/src/main/java/dean/weather/locationsFrag.java
@@ -1,15 +1,20 @@
 package dean.weather;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by DeanF on 9/16/2016.
  */
 public class locationsFrag extends Fragment {
+    TextView currentLocationTemp;
+    TextView currentLocationCondition;
+    Typeface robotoLight;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.locations_fragment, container, false);
@@ -18,5 +23,19 @@ public class locationsFrag extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //Setup references
+        currentLocationTemp = (TextView) getView().findViewById(R.id.locationsCurrentTemp);
+        currentLocationCondition = (TextView) getView().findViewById(R.id.locationsCurrentConditions);
+        robotoLight = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Light.ttf");
+
+        //Set font
+        currentLocationTemp.setTypeface(robotoLight);
+        currentLocationCondition.setTypeface(robotoLight);
+
     }
 }
