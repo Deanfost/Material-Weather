794bef5c8764b05c1498cbec155e811aee645768
Dean Foster <Deanfoster11@gmail.com>
2016-10-30 16:49:18 -0400

    Finished onboarding slides 2 and 3, added permissionsfragment

---
+++ Weather/mobile/src/main/java/dean/weather/onBoardingFragThree.java
@@ -0,0 +1,34 @@
+package dean.weather;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by DeanF on 10/30/2016.
+ */
+
+public class onBoardingFragThree extends Fragment {
+    Button btnRequestPermissions;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.onboarding_frag_three, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnRequestPermissions = (Button) getView().findViewById(R.id.btnOnboardingPermission);
+
+        btnRequestPermissions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onboardingActivity.requestPermissions();
+            }
+        });
+    }
+}
--- Weather/mobile/src/main/java/dean/weather/onboardingActivity.java
+++ Weather/mobile/src/main/java/dean/weather/onboardingActivity.java
@@ -1,22 +1,41 @@
 package dean.weather;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.app.NavigationPolicy;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
-import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 /**
  * Created by DeanF on 10/29/2016.
  */
 
 public class onboardingActivity  extends IntroActivity{
+    static int LOCATION_PERMISSIONS_REQUEST = 42;
+    private Fragment onBoardingFragThree;
+    private Class onBoardingFragThreeClass;
+    public static Activity currentActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        currentActivity = this;
+        try {
+            onBoardingFragThreeClass = onBoardingFragThree.class;
+            onBoardingFragThree = (Fragment) onBoardingFragThreeClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
         //Create slide 1
         addSlide(new FragmentSlide.Builder()
@@ -32,6 +51,15 @@ public class onboardingActivity  extends IntroActivity{
                 .fragment(R.layout.onboarding_frag_two)
                 .build());
 
+        //Create slide 3
+        addSlide(new FragmentSlide.Builder()
+                .backgroundDark(R.color.colorBlue)
+                .background(R.color.colorBlueLight)
+                .fragment(onBoardingFragThree)
+                .build());
+
+        setSkipEnabled(false);
+
         setNavigationPolicy(new NavigationPolicy() {
             @Override
             public boolean canGoForward(int position) {
@@ -42,6 +70,8 @@ public class onboardingActivity  extends IntroActivity{
                         return true;
                     case 2:
                         return false;
+                    case 3:
+                        return true;
                 }
                 return true;
             }
@@ -55,9 +85,33 @@ public class onboardingActivity  extends IntroActivity{
                         return true;
                     case 2:
                         return true;
+                    case 3:
+                        return true;
                 }
                 return false;
             }
         });
+
+    }
+
+    //Handle request results
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //If the user didn't cancel the dialog
+        if (grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                //If the user allowed access, then launch the main activity
+                Intent startMain = new Intent(this, MainActivity.class);
+                startActivity(startMain);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        }
+    }
+
+    public static void requestPermissions(){
+        ActivityCompat.requestPermissions(currentActivity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSIONS_REQUEST);
     }
 }
